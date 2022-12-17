507ec6d07552d71c3929399374c48a687e1ab4e9
Valdenir Severino <vald3nir@gmail.com>
2022-12-17 13:29:35 -0300

    Create Constants.dart

---
+++ smart_energy_flutter/lib/src/app/Constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static String appName = "Smart Energy";
+
+  // Text Sizes
+  static double textTitleSize = 28.0;
+
+  // Images Path
+  static String logoPath = "assets/images/app_logo.png";
+}
